a756136c7a5c4b26d134a5218dffa224b222d2b5
Afif Uddin <afifuddin@192.168.1.5>
2024-05-29 10:14:27 +0700

    initial

--- App.js
+++ App.js
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
+// Flexbox
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={styles.layout}>
+
+      {/* Flex Direction */}
+      {/* Flex Direction : row */}
+      {/* <View style={{ flexDirection: 'row' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View> */}
+
+      {/* Flex Direction : column */}
+      {/* <View style={{ flexDirection: 'column' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View> */}
+
+      {/* Flex Direction : row-reverse */}
+      {/* <View style={{ flexDirection: 'row-reverse' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View> */}
+
+      {/* Flex Direction : column-reverse */}
+      {/* <View style={{ flexDirection: 'column-reverse' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View> */}
+
+      {/* Justify Content */}
+      {/* <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View> */}
+
+      {/* alignItems  */}
+      <View style={{ alignItems: 'flex-start' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+      <View style={{ alignItems: 'flex-end' }}>
+        <View style={styles.biru} />
+        <View style={styles.merah} />
+        <View style={styles.hijau} />
+      </View>
+
+
+    </View >
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
+    marginTop: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  biru: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'blue'
+  },
+  merah: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red'
+  },
+  hijau: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green'
+  },
+})
